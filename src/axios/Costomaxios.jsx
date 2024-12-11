903aa67f137bd6eaa1f76ab7ac87eee16a8160e6
abhi <hgabishek441@gmail.com>
2024-12-11 16:07:49 +0530

    Initial commit

--- src/axios/Costomaxios.jsx
+++ src/axios/Costomaxios.jsx
@@ -1,14 +1,26 @@
 import axios from "axios";
 
+// const CustomFetch = axios.create({
+//   baseURL: "http://localhost:5000",
+//   headers: {
+//     Accept: "application/json",
+//     // Authorization: `Bearer ${token}`,
+//   },
+//   withCredentials: true,
+// });
+
+// const productUrl = "http://localhost:5000";
+
+// Use the hosted backend URL for the production environment
 const CustomFetch = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ayurbharatbackend.onrender.com", // Updated URL
   headers: {
     Accept: "application/json",
-    // Authorization: `Bearer ${token}`,
+    // Authorization: `Bearer ${token}`, // Uncomment and use token if needed
   },
-  withCredentials: true,
+  withCredentials: true, // Ensures cookies are sent with the request
 });
 
-const productUrl = "http://localhost:5000";
+// const productUrl = "https://ayurbharatbackend.onrender.com"; // Updated URL
 
 export { CustomFetch };
